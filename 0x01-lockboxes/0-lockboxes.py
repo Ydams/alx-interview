8963294594c01f3b8dbb9a7b9ec85210a467e1b5
ydams <damola4542@gmail.com>
2024-11-12 10:29:50 +0100

    lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
-def can_unlock_all(boxes):
+def canUnlockAll(boxes):
     """
     Determine if all boxes can be unlocked.
 
     :param boxes: List of lists where each list contains keys to other boxes.
     :return: True if all boxes can be unlocked, False otherwise.
     """
-    n = len(boxes)
-    unlocked = [False] * n  # List to track unlocked boxes
-    unlocked[0] = True  # The first box is unlocked
-    keys = boxes[0]  # Start with the keys in the first box
-    unlocked_count = 1  # Number of unlocked boxes
-
-    while keys:
-        key = keys.pop()  # Take one key at a time
-        if key < n and not unlocked[key]:  # Check if the key is valid and the box is locked
-            unlocked[key] = True  # Unlock the box
-            unlocked_count += 1  # Increment the unlocked box count
-            keys.extend(boxes[key])  # Add new keys from this box to the key list
-
-    return unlocked_count == n  # Return True if all boxes are unlocked, False otherwise
-
+    """Determines if all the boxes can be opened"""
+    if (type(boxes) is not list or len(boxes) == 0):
+        return False
+    for k in range(1, len(boxes) - 1):
+        unlocked = False
+        for i in range(len(boxes)):
+            unlocked = k in boxes[i] and k != i
+            if unlocked:
+                break
+        if unlocked is False:
+            return unlocked
+    return True
